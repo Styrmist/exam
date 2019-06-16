ac5a91a952fd0fef8ba659c65121ea39b1d68c61
laziest <_limitless@tuta.io>
2019-06-16 23:12:33 +0300

    Initial Commit

---
+++ exam-bare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exam-bare
+//
+//  Created by Kirill on 6/16/19.
+//  Copyright © 2019 Kirill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
